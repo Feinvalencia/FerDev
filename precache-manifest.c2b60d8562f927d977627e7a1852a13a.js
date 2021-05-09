ad587d4d56ba98880293c583fa272e8b67a9202b
feiner <feinvalencia@gmail.com>
2021-05-08 19:08:29 -0500

    Updates

---
+++ precache-manifest.c2b60d8562f927d977627e7a1852a13a.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8893c1b3a998e60308a2e743f21d868b",
+    "url": "/el-nucleo/index.html"
+  },
+  {
+    "revision": "2575c2d58032980ec4a2",
+    "url": "/el-nucleo/static/css/main.c0f422e7.chunk.css"
+  },
+  {
+    "revision": "e1bc235231797147ae88",
+    "url": "/el-nucleo/static/js/2.afff8edf.chunk.js"
+  },
+  {
+    "revision": "2df1482e1a3ba2e21b99e30b34c2b08f",
+    "url": "/el-nucleo/static/js/2.afff8edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2575c2d58032980ec4a2",
+    "url": "/el-nucleo/static/js/main.0a907f91.chunk.js"
+  },
+  {
+    "revision": "c8edc95702673225cac72410bdd8f712",
+    "url": "/el-nucleo/static/js/main.0a907f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c190ce37b53fadad27c",
+    "url": "/el-nucleo/static/js/runtime-main.cfbf19ca.js"
+  },
+  {
+    "revision": "6c0e53e35179da3df2a93593701032cc",
+    "url": "/el-nucleo/static/media/FINANCE.6c0e53e3.jpg"
+  },
+  {
+    "revision": "988e0f8701a6a1a3f2a941d39c29e1d5",
+    "url": "/el-nucleo/static/media/Graphik-Medium.988e0f87.ttf"
+  },
+  {
+    "revision": "475fb1f8332e3a4a5364d0cc76d2a9cd",
+    "url": "/el-nucleo/static/media/Graphik-Regular.475fb1f8.woff"
+  },
+  {
+    "revision": "dd1570136d6f325e349b6b0b14cc82e6",
+    "url": "/el-nucleo/static/media/HEALTH.dd157013.jpg"
+  },
+  {
+    "revision": "b6f9e13160349542c3be9f1fe276c712",
+    "url": "/el-nucleo/static/media/HOSPITALITY.b6f9e131.jpg"
+  },
+  {
+    "revision": "dac6e4e5a8ed751f064d1a3e9ab5d52d",
+    "url": "/el-nucleo/static/media/MEDIA.dac6e4e5.jpg"
+  },
+  {
+    "revision": "a913e212dd34d22db02b5fc8099b3226",
+    "url": "/el-nucleo/static/media/MOBILE.a913e212.jpg"
+  },
+  {
+    "revision": "3e771f886ec047f336eca11664b84823",
+    "url": "/el-nucleo/static/media/Matrice-Bold.3e771f88.ttf"
+  },
+  {
+    "revision": "ddaf072d20498cd51391ad2d826d538f",
+    "url": "/el-nucleo/static/media/Matrice-Medium.ddaf072d.ttf"
+  },
+  {
+    "revision": "8e9308362581d6ba29f95af7adb28708",
+    "url": "/el-nucleo/static/media/Prestige-Signature-Script-Demo.8e930836.ttf"
+  },
+  {
+    "revision": "f073a390dfc48800ad30cc8256fcaeae",
+    "url": "/el-nucleo/static/media/REALESTATE.f073a390.jpg"
+  },
+  {
+    "revision": "6198b2081d4a175781b20b9d1d6f766c",
+    "url": "/el-nucleo/static/media/RETAIL.6198b208.jpg"
+  },
+  {
+    "revision": "dc25dfa0601c4bc09077820dc057db0c",
+    "url": "/el-nucleo/static/media/WEB-APP.dc25dfa0.jpg"
+  },
+  {
+    "revision": "d0b18363bcc236fecee4e7b948021d31",
+    "url": "/el-nucleo/static/media/arrow.d0b18363.svg"
+  },
+  {
+    "revision": "6cee2ba5dd54d07c29f25eed6cf99adb",
+    "url": "/el-nucleo/static/media/binaryblocks.6cee2ba5.mp4"
+  },
+  {
+    "revision": "58bbe505d5acb3a6625d293d8734eb66",
+    "url": "/el-nucleo/static/media/birds.58bbe505.mp4"
+  },
+  {
+    "revision": "4019fe6478312c4103b3d6f6b1c33f5c",
+    "url": "/el-nucleo/static/media/celebrate.4019fe64.jpg"
+  },
+  {
+    "revision": "582fd1b635113c908a1a35112d20e91f",
+    "url": "/el-nucleo/static/media/centralized.582fd1b6.jpg"
+  },
+  {
+    "revision": "f30e3c2d017f6feb716e9d162540b05e",
+    "url": "/el-nucleo/static/media/cover.f30e3c2d.mp4"
+  },
+  {
+    "revision": "e3173046c95a4a8b907bd181f9e0646f",
+    "url": "/el-nucleo/static/media/datasecurity.e3173046.jpg"
+  },
+  {
+    "revision": "43073f61064f3ec8526664d90fc9f4f2",
+    "url": "/el-nucleo/static/media/datos.43073f61.mp4"
+  },
+  {
+    "revision": "7a034c463331651e71989d4d064844d3",
+    "url": "/el-nucleo/static/media/go.7a034c46.jpg"
+  },
+  {
+    "revision": "efd7e1a0ea10c8b992e9fb6973c9b20e",
+    "url": "/el-nucleo/static/media/goals.efd7e1a0.jpg"
+  },
+  {
+    "revision": "968c0e1ef9c89f15e4ad44fd4e3bd52d",
+    "url": "/el-nucleo/static/media/levitation.968c0e1e.jpg"
+  },
+  {
+    "revision": "797ec2ff28c3168b0982f77ab9464f05",
+    "url": "/el-nucleo/static/media/logo_small.797ec2ff.svg"
+  },
+  {
+    "revision": "845835df2423bcedd3830af30ec5cc89",
+    "url": "/el-nucleo/static/media/logos.845835df.svg"
+  },
+  {
+    "revision": "463ed7d9b0726b580a8e239ef4556a4b",
+    "url": "/el-nucleo/static/media/magic.463ed7d9.jpg"
+  },
+  {
+    "revision": "b0eb540a5b04bb15c688645ed2ebaeea",
+    "url": "/el-nucleo/static/media/modular.b0eb540a.jpg"
+  },
+  {
+    "revision": "481873394d6601f2b7e2e4682624193a",
+    "url": "/el-nucleo/static/media/piramide.48187339.mp4"
+  },
+  {
+    "revision": "97c9d928e9bf51b29cbd115e1cb9bdcd",
+    "url": "/el-nucleo/static/media/play.97c9d928.svg"
+  },
+  {
+    "revision": "a4d1be0bf0e920fa9be3524df2237e18",
+    "url": "/el-nucleo/static/media/portfolio.a4d1be0b.jpg"
+  },
+  {
+    "revision": "6c894811c842b742accc34eeade576b1",
+    "url": "/el-nucleo/static/media/titanic.6c894811.png"
+  }
+]);
